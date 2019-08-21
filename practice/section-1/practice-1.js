d2dbd97520a73562382bcfaade3bca81074c6c28
SKB <1215237417@qq.com>
2019-08-21 10:23:39 +0800

    add

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,15 @@
 'use strict';
 
+
+  
 function find(collection, element) {
-  return '实现练习要求，并改写该行代码。';
+  let result;
+for(let i=0; i<collection.length; i++){
+if(collection[i]===element){
+return result=collection[i];
 }
+}
+return '查无此数';
+}
+
+ 
